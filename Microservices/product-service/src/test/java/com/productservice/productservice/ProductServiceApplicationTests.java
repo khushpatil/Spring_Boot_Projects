9b21a37893841f756faaaea6fac5363b9eb17d28
khushpatil <khushpatil70@gmail.com>
2023-02-15 12:09:39 +0530

    Restructured the project into an empty maven project and verified it

--- Microservices/product-service/src/test/java/com/productservice/productservice/ProductServiceApplicationTests.java
+++ Microservices/product-service/src/test/java/com/productservice/productservice/ProductServiceApplicationTests.java
@@ -1,60 +1,15 @@
 package com.productservice.productservice;
 
-import java.math.BigDecimal;
-
-import org.springframework.http.MediaType;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.productservice_microservices.productservice.dto.ProductRequest;
+import com.productservice_microservices.productservice.ProductServiceApplication;
 
-@SpringBootTest
-@Testcontainers
-@AutoConfigureMockMvc
+@SpringBootTest(classes = ProductServiceApplication.class)
 class ProductServiceApplicationTests {
 
-	@Container
-	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
-
-	@Autowired
-	private MockMvc mockMvc;
-
-	@Autowired
-	private ObjectMapper objMapper;
-
-	@DynamicPropertySource
-	static void setProperties(DynamicPropertyRegistry dpr) {
-		dpr.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
-	}
-
 	@Test
-	void shouldCreateProduct() throws Exception {
-		ProductRequest prodReq = getProductRequest();
-		String prodReqString = objMapper.writeValueAsString(prodReq);
-
-		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(prodReqString))
-				.andExpect(status().isCreated());
-	}
-
-	private ProductRequest getProductRequest() {
-		return ProductRequest.builder()
-				.name("Iphone 13")
-				.desc("Latest Iphone")
-				.price(BigDecimal.valueOf(2500))
-				.build();
+	void contextLoads() {
 	}
 
 }
